bb85fe3c5d02cf16c36cefdbf6744e4c54c33574
arercon <jannekschroeter@gmail.com>
2024-09-15 17:14:01 +0200

    added inside level

--- scenes/levels/outside.gd
+++ scenes/levels/outside.gd
@@ -1,7 +1,9 @@
 extends LevelParent
 
-
+var inside_scene: PackedScene = preload("res://scenes/levels/inside.tscn")
 
 func _on_gate_player_entered_gate(_body: Variant) -> void:
 	var tween = create_tween()
 	tween.tween_property($Player, "speed", 0, 0.7)
+	#get_tree().call_deferred("change_scene_to_file", "res://scenes/levels/inside.tscn")
+	get_tree().call_deferred("change_scene_to_packed", inside_scene)
