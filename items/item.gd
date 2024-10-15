9f18772372f96e5c25347cb8ee5eeeb0eefd1f98
arercon <jannekschroeter@gmail.com>
2024-10-15 22:04:47 +0200

    Crates, toilets, infestation and textures added to inside level

--- items/item.gd
+++ items/item.gd
@@ -4,6 +4,9 @@ var rotation_speed = 4
 var available_options = ['laser','laser','laser', 'grenade', 'health']
 var type = available_options[randi()%len(available_options)]
 
+var direction: Vector2
+var distance: int = randi_range(150,250)
+
 func _ready() -> void:
 	if type == 'laser':
 		$Sprite2D.modulate = Color(0.1,0.2,0.8)
@@ -12,10 +15,17 @@ func _ready() -> void:
 	if type == 'health':
 		$Sprite2D.modulate = Color(0.1,0.8,0.1)
 		
+	#tween
+	var target_pos = position + direction * distance
+	var tween = create_tween()
+	tween.set_parallel(true)
+	tween.tween_property(self, "position", target_pos, 0.5)
+	tween.tween_property(self, "scale", Vector2(1,1), 0.3).from(Vector2(0,0))
+		
 func _process(delta: float) -> void:
 	rotation += rotation_speed*delta
 	
-func _on_body_entered(body) -> void:
+func _on_body_entered(_body) -> void:
 	if type == 'health':
 		Globals.health += 10
 	if type == 'laser':
--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -3,8 +3,17 @@ class_name LevelParent
 
 var laser_scene: PackedScene = preload("res://scenes/projectiles/laser.tscn")
 var grenade_scene: PackedScene = preload("res://scenes/projectiles/grenade.tscn")
+var item_scene: PackedScene = preload("res://items/item.tscn")
 	
-
+func _ready() -> void:
+	for container in get_tree().get_nodes_in_group("Container"):
+		container.connect("open", _on_container_opened)
+		
+func _on_container_opened(pos, direction):
+	var item = item_scene.instantiate()
+	item.position = pos
+	item.direction = direction
+	$Items.call_deferred('add_child', item)
 
 func _on_player_laser_has_been_shot(pos, direction):
 	var laser = laser_scene.instantiate() as Area2D
@@ -12,7 +21,6 @@ func _on_player_laser_has_been_shot(pos, direction):
 	laser.rotation_degrees = rad_to_deg(direction.angle()) + 90
 	laser.direction = direction
 	$Projectiles.add_child(laser)
-	$UI.update_laser_text()
 	
 	
 func _on_player_grande_has_been_thrown(pos, direction):
@@ -20,22 +28,3 @@ func _on_player_grande_has_been_thrown(pos, direction):
 	grenade.position = pos
 	grenade.linear_velocity = direction * grenade.speed
 	$Projectiles.add_child(grenade)
-	$UI.update_grenade_text()
-
-
-func _on_house_player_entered() -> void:
-	var tween = get_tree().create_tween()
-	tween.set_parallel(true)
-	tween.tween_property($Player/Camera2D, "zoom", Vector2(1,1), 1).set_trans(Tween.TRANS_QUAD)
-	#tween.tween_property($Player, "modulate:a", 0,2).from(0.5)
-
-
-func _on_house_player_exited() -> void:
-	var tween = get_tree().create_tween()
-	tween.tween_property($Player/Camera2D, "zoom", Vector2(0.6, 0.6), 1)
-
-
-func _on_player_update_stats() -> void:
-	$UI.update_laser_text()
-	$UI.update_grenade_text()
-	
