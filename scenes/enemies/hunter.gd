ddbb7494f049b56eb541fc6501ce19311923c21d
arercon <jannekschroeter@gmail.com>
2024-12-01 21:48:19 +0100

    Added hunter (Pathfinding still buggy)

---
+++ scenes/enemies/hunter.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+var active: bool = false
+var speed: int = 200
+
+func _ready() -> void:
+	await get_tree().create_timer(0.1).timeout
+	$NavigationAgent2D.target_position = Globals.player_pos
+
+	
+func _physics_process(_delta: float) -> void:
+	if active:
+		var next_path_pos: Vector2 = $NavigationAgent2D.get_next_path_position()
+		var direction: Vector2 = (next_path_pos - global_position).normalized()
+		velocity = direction * speed
+		move_and_slide()
+		look_at(Globals.player_pos)
+	
+
+
+func _on_notice_area_body_entered(_body: Node2D) -> void:
+	active = true
+
+
+func _on_notice_area_body_exited(_body: Node2D) -> void:
+	active = false
+
+
+func _on_navigation_timer_timeout() -> void:
+	if active:
+		$NavigationAgent2D.target_position = Globals.player_pos
