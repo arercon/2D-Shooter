c0b6db68acb24a5a55814ad6d08d3457def21587
arercon <jannekschroeter@gmail.com>
2024-12-13 00:01:58 +0100

    Hunter finished

--- scenes/enemies/hunter.gd
+++ scenes/enemies/hunter.gd
@@ -1,10 +1,15 @@
 extends CharacterBody2D
 
 var active: bool = false
+var player_near: bool = false
 var speed: int = 200
+var vulnerable: bool = true
+var health: int = 100
 
 func _ready() -> void:
 	await get_tree().create_timer(0.1).timeout
+	$NavigationAgent2D.path_desired_distance = 4.0
+	$NavigationAgent2D.target_desired_distance = 4.0
 	$NavigationAgent2D.target_position = Globals.player_pos
 
 	
@@ -14,12 +19,15 @@ func _physics_process(_delta: float) -> void:
 		var direction: Vector2 = (next_path_pos - global_position).normalized()
 		velocity = direction * speed
 		move_and_slide()
-		look_at(Globals.player_pos)
+		var look_angle = direction.angle()
+		rotation = look_angle + PI / 2
+		
 	
 
 
 func _on_notice_area_body_entered(_body: Node2D) -> void:
 	active = true
+	$AnimationPlayer.play("walk")
 
 
 func _on_notice_area_body_exited(_body: Node2D) -> void:
@@ -29,3 +37,25 @@ func _on_notice_area_body_exited(_body: Node2D) -> void:
 func _on_navigation_timer_timeout() -> void:
 	if active:
 		$NavigationAgent2D.target_position = Globals.player_pos
+
+
+func _on_attack_area_body_entered(body: Node2D) -> void:
+	player_near = true
+	$AnimationPlayer.play("attack")
+
+func _on_attack_area_body_exited(body: Node2D) -> void:
+	player_near = false
+	
+func attack():
+	if player_near:
+		Globals.health -= 20
+
+func hit():
+	if vulnerable:
+		health -= 10
+		$Timers/HitTimer.start()
+	if health <= 0:
+		queue_free()
+
+func _on_hit_timer_timeout() -> void:
+	vulnerable = true
